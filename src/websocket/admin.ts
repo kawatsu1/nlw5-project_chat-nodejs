14b2ad4eb40c1ab69fddb7853e56ceae24efeff3
André Kawatsu <kawatsu1066@gmail.com>
2021-04-24 14:29:55 -0300

    Aula 5 - Final

--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -34,4 +34,14 @@ io.on("connect", async (socket) => {
 			socket_id: socket_id,
 		});
 	});
+
+	socket.on("admin_user_in_support", async (params) => {
+		const { user_id } = params;
+
+		await connectionsService.updateAdminID(user_id, socket.id);
+
+		const allConnectionWithoutAdmin = await connectionsService.findAllWithoutAdmin();
+
+		io.emit("admin_list_all_users", allConnectionWithoutAdmin);
+	});
 });
