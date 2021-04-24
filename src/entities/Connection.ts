78bfac09f3eb7b1c834c00bd715a53208c071611
André Kawatsu <kawatsu1066@gmail.com>
2021-04-24 02:27:40 -0300

    Aula 5 Admin

--- src/entities/Connection.ts
+++ src/entities/Connection.ts
@@ -10,7 +10,7 @@ import {
 import { v4 as uuid } from "uuid";
 import { User } from "./User";
 
-@Entity("connections")
+@Entity("Connections")
 class Connection {
 	@PrimaryColumn()
 	id: string;
--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -50,6 +50,9 @@ io.on("connect", (socket) => {
 			text,
 			user_id,
 		});
-		// Salvar a conexão  com o socket_id, user_id
+
+		const allMessages = await messagesService.listByUser(user_id);
+
+		socket.emit("client_list_all_messages", allMessages);
 	});
 });
